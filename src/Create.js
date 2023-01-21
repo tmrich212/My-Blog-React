32955517b9d6fa3a8f96c6caa054dd10f0189f81
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-01-21 18:04:23 -0500

    form and functionality added to our new post page

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,46 @@
+import { useState } from 'react';
+// import { onChange } from 'react';
+
 const Create = () => {
+
+    const [title, setTitle]= useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Taylor');
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog = { title, body, author };
+        console.log(blog);
+    }
+
+
     return ( 
-        <h2>Add A New Blog Post</h2>
+        <div className="create">
+            <h2>Add A New Blog Post</h2>
+            <form onSubmit={ handleSubmit }>
+                <label htmlFor="">Blog Title:</label>
+                <input type="text"
+                value={ title }
+                onChange={(e) => setTitle(e.target.value)} required/>
+
+                <label htmlFor="">Blog Body:</label>
+                <textarea 
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                required></textarea>
+
+                <label htmlFor="">Blog Author:</label>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Taylor">Taylor</option>
+                    <option value="New User">New User</option>
+                </select>
+
+                <button>Add Blog</button>
+                {/* <p>{title}</p> this is a test! our onChange is working */}
+            </form>
+        </div>
      );
 }
  
