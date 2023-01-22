7fc731dfc6c13a84373ad22371cad2671a922faa
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-01-21 21:33:58 -0500

    added a deletion option for blogs and a 404 page

--- src/Create.js
+++ src/Create.js
@@ -1,16 +1,34 @@
 import { useState } from 'react';
-// import { onChange } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
 
     const [title, setTitle]= useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Taylor');
+
+    const [isPending, setIsPending] = useState(false);
+
+    const history = useHistory();
+
+
     const handleSubmit = (e) =>{
         e.preventDefault();
         const blog = { title, body, author };
-        console.log(blog);
+        
+        fetch('http://localhost:8000/blogs/', {
+            method: 'POST',
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify(blog)
+        })
+        .then(() => {
+            console.log('new blog added');
+            setIsPending(true);
+            history.push('/');
+        })
     }
+
+   
 
 
     return ( 
@@ -37,7 +55,8 @@ const Create = () => {
                     <option value="New User">New User</option>
                 </select>
 
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button>}
+                { isPending && <button disabled>Adding Blog...</button>}
                 {/* <p>{title}</p> this is a test! our onChange is working */}
             </form>
         </div>
