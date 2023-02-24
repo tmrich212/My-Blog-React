169d67e6598f46150ed1dba959904bdda3571706
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-02-23 22:20:54 -0500

    Code Insertion Auth test removed

--- src/Create.js
+++ src/Create.js
@@ -1,37 +1,6 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-//NEW CODE INSERTION
-import React from "react";
-import ReactDOM from "react-dom";
-
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      password: "swordfish",
-      authorized: false
-    };
-    this.authorize = this.authorize.bind(this);
-  }
-
-  authorize(e) {
-    const password = e.target.querySelector('input[type="password"]').value;
-    const auth = password == this.state.password;
-    this.setState({
-      authorized: auth,
-    });
-  }
-  render() {
-    const login = (
-      <form action="#"
-      onSubmit={this.authorize}>
-        <input type="password" placeholder="Password" />
-        <input type="submit" />
-      </form>
-    );
-
-//END OF CODE INSERTION
 
 const Create = () => {
 
